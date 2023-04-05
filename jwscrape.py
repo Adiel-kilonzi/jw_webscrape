aabc5aae0f66964783090b26fc31a94408847abc
Adiel-kilonzi <adlkilonzi@gmail.com>
2023-04-05 07:09:19 +0300

    did a bit of cleanup

--- jwscrape.py
+++ jwscrape.py
@@ -3,15 +3,17 @@ import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
-#import datetime
 from datetime import datetime, timedelta, date
 
-
+#global variables
 dfDailyText = pd.DataFrame(columns=['date','scripture','comments'])
 baseLink = "https://wol.jw.org/en/wol/h/r1/lp-e/"
+
+# function or method that takes the date and dataframe to append it fetches and appends for tthe date given
 def getDailyText(getDate, dfDailyText):
-    linkDate = '{dt.year}/{dt.month}/{dt.day}'.format(dt = getDate)
+    linkDate = '{dt.year}/{dt.month}/{dt.day}'.format(dt = getDate) #converts the date to the link format removing zeros
     link = baseLink + linkDate
+    #The actual request
     try:
         result = requests.get(link)
         linkResult = result.content
@@ -23,42 +25,30 @@ def getDailyText(getDate, dfDailyText):
     else:
         print("Parsing for date " + getDate.strftime('%Y/%m/%d'))
 
-    dailyText = page.find('div', attrs = {'class':'articlePositioner'})
+    dailyText = page.find('div', attrs = {'class':'articlePositioner'}) #locating the actual dailytext
 
-    scripture = dailyText.find_all('p', attrs = {'class':'themeScrp'})
+    scripture = dailyText.find_all('p', attrs = {'class':'themeScrp'}) 
     watchtowerComments = dailyText.find_all('p', attrs = {'class':'sb'})
-
-    #previousDaylink = navigationlinks.find('a', attrs = {'aria-label':'previous day'}).get("href")
+    #scripture has 2 parts the scripture text and quotation
     scriptureem = scripture[1].find_all('em')
-
+    
     scriptureString = ""
-    #scriptureString
     for comments in scriptureem:
         scriptureString += str(comments.get_text())
-        #print(comments.get_text())
-    #print("\n scripture is " + scriptureString)    
+    #picking comments for the day        
     wComments = watchtowerComments[1].get_text()
+    #appending the data for the day on the dataframe using append() causes warning
     row = pd.Series([getDate, scriptureString, wComments], index=dfDailyText.columns)
     dfDailyText = dfDailyText.append(row, ignore_index=True)
-    #dfDailyText = pd.concat([dfDailyText, row])
-    print("Finished date " + getDate.strftime('%Y/%m/%d'))
-    #print(dfDailyText.head())
+    print("Finished date " + getDate.strftime('%Y/%m/%d')) #alert that date has been parsed
     return (dfDailyText)
 
 
-    #print(scripture[1].em.get_text())
-    #print(scripture[1].em.[1].get_text())
-    #print ("\n")
-    #print(watchtowerComments[1].get_text())
-#'{dt.year}/{dt.month}/{dt.day}'.format(dt = mydate)
-
-
-#fromDate = datetime.strptime(input("Please enter from date in yyyy/mm/dd : "),'%Y/%m/%d')
-#toDate = datetime.strptime(input("Please enter to date in yyyy/mm/dd : "), '%Y/%m/%d')
+#code starts running from below
+#date range is defined here
 fromDate = datetime.strptime("2023/04/01",'%Y/%m/%d')
 toDate = datetime.strptime("2023/04/04",'%Y/%m/%d')
-#outputFile = input("Please Enter output file name : ")
-#toDate =+ timedelta(days=1)
+
 if( fromDate > toDate):
     print ("ERROR: From Date cannot be greater than to date")
     exit(1)
